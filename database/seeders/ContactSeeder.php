b4fc0dcfcdece6433f69e66522116f658d32c011
buenexx <buenofernando2013@gmail.com>
2022-04-28 10:47:38 -0300

    contacts seeder ok

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ContactSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-         \App\Models\Contact::factory(10)->create();
+        //
     }
 }
