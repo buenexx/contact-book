6f43665d8490df22532bc08c02a6132f59de7ef0
buenexx <buenofernando2013@gmail.com>
2022-04-28 12:59:35 -0300

    form update ok

--- app/Http/Controllers/ContactUpdateController.php
+++ app/Http/Controllers/ContactUpdateController.php
@@ -9,10 +9,12 @@ class ContactUpdateController extends Controller
 {
     public function __invoke(Contact $contact)
     {
+       // TODO: create validation for the email to remain unique when it is updated.
+
         request()->validate([
             'name' => 'required|min:5|max:255',
-            'email' => 'required|email|max:100|unique:contacts',
-            'phone' => 'required|min:9|max:21|unique:contacts',
+            'email' => 'required|email|max:100',
+            'phone' => 'required|min:9|max:21',
         ]);
 
         $contact->fill([
